54f4048fd4f38aefa8ebdab74cb8176834995396
K Guna Shaker <gunashaker45k@gmail.com>
2025-02-27 22:21:51 +0530

    Some Basics...

---
+++ com/basics/GFG.java
@@ -0,0 +1,15 @@
+/*"A simple Java program that prints 'GeeksForGeeks' to the console. " +
+        "The 'Hello World' print statement is commented out," +
+        " demonstrating how to disable specific lines of code without deleting them."
+*/
+package com.basics;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Prints Hello World
+        //System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
---
+++ com/basics/Geeks.java
@@ -0,0 +1,27 @@
+/*"A simple Java function that prints two integers while skipping the second one. " +
+        "The function takes three parameters (a, b, and c)
+        and prints only 'a' and 'c' by commenting out the line that outputs 'b'.
+        Useful for understanding how to selectively print values in Java."
+ */
+
+
+package com.basics;
+//User function Template for Java
+
+//Back-end complete function Template for Java
+
+class Geeks{
+
+    // Function to print a and c
+    //comment the line that outputs b
+    static void comment(int a, int b, int c){
+
+        //Prints a
+        System.out.println(a);
+        //Prints b
+        //System.out.println(b);
+        //Prints c
+        System.out.println(c);
+    }
+
+}
