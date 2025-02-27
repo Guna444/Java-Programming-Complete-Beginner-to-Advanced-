724e4c32b8cf28272a293e96be877851291fafb6
K Guna Shaker <gunashaker45k@gmail.com>
2025-02-27 22:11:53 +0530

    A simple Hello World Program.

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+/*"A classic 'Hello, World!' program written in Java.
+This basic script prints 'Hello World' to the console,
+serving as a simple introduction to Java programming."
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("'Hello, World!'");
+    }
+}
