0a070b45627f998a3601f78d5b0d554cddd01c35
K Guna Shaker <gunashaker45k@gmail.com>
2025-03-08 11:58:33 +0530

    Taking an integer as input and printing by adding 10

---
+++ com/input/InputInteger.java
@@ -0,0 +1,15 @@
+package com.input;
+
+   import java.util.*;
+
+    class InputInteger {
+        public static void main(String[] args) {
+            // Implement Scanner Class
+            //  Take input
+            //  Add 10 and Print
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            System.out.println(n+10);
+
+        }
+    }
