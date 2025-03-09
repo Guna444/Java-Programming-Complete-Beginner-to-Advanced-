88ba1e05a737af5b11e09d05292e61107e107a34
K Guna Shaker <gunashaker45k@gmail.com>
2025-03-09 18:27:41 +0530

    Taking a String as an input and printing it.

---
+++ com/input/InputString.java
@@ -0,0 +1,17 @@
+package com.input;
+
+
+import java.util.*;
+
+public class InputString {
+    public static void main(String args[]) {
+        // Take String Input and Print it
+        //User function Template for Java
+// BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+//Complete the below code
+        Scanner sc = new Scanner(System.in);
+        String myInput = sc.nextLine();
+//Complete the below code
+        System.out.println(myInput);
+    }
+}
