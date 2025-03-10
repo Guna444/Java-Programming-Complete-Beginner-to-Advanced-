d22858c4f67de4b1228852e72c1ab702810752cd
K Guna Shaker <gunashaker45k@gmail.com>
2025-03-10 20:52:40 +0530

    Taking a Float as an input and printing it by multiplying 10.

---
+++ com/input/InputFloat.java
@@ -0,0 +1,12 @@
+package com.input;
+
+import java.util.*;
+
+public class InputFloat {
+        public static void main(String args[]) {
+            // Take a Float input and Print
+            Scanner scanner = new Scanner(System.in);
+            float n = scanner.nextFloat();
+            System.out.println(n*10);
+        }
+    }
